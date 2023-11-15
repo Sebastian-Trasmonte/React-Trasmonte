7a045f54dac754a53d46955e9a15cb5f7722b7a0
Sebastian Trasmonte <sebas.trasmonte@gmail.com>
2023-11-14 21:52:08 -0300

    agrego props

---
+++ src/components/Body/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+function ItemListContainer(props) {
+    const {saludo}=props;
+    return (
+        
+        <div>
+            <h1>{saludo}</h1>
+        </div>
+    )
+}
+export default ItemListContainer
